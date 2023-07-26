da6aa697f453f51dbd281eb40d81051b11b46ce2
eileenjjy <dosajjy@naver.com>
2023-07-27 01:28:56 +0900

    위시, 컬렉션 뒤로가기 버튼 구현

--- app/src/main/java/com/example/nota/WriteCollectionActivity.kt
+++ app/src/main/java/com/example/nota/WriteCollectionActivity.kt
@@ -2,6 +2,7 @@ package com.example.nota
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 
 class WriteCollectionActivity : AppCompatActivity() {
 
@@ -12,5 +13,9 @@ class WriteCollectionActivity : AppCompatActivity() {
         // 여기에 WriteCollectionActivity에 필요한 추가적인 설정이나 로직을 작성하세요.
         // 예를 들어, EditText, 버튼 등의 UI 구성 요소를 추가하거나 필요한 작업을 처리할 수 있습니다.
         // 또는 사용자 상호작용을 처리하고 필요한 데이터를 저장하는 등의 작업을 수행할 수 있습니다.
+    }
+
+    fun onBackButtonClicked(view: View) {
+        finish()
     }
 }
--- app/src/main/java/com/example/nota/WriteWishActivity.kt
+++ app/src/main/java/com/example/nota/WriteWishActivity.kt
@@ -2,10 +2,14 @@ package com.example.nota
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class WriteWishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.write_wish)
+    }
+    fun onBackButtonClicked(view: View) {
+        finish()
     }
 }
