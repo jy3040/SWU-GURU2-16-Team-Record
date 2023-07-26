c35b9710ea59c04cad828836e4515d9663b067d1
J3214 <hjm66336831@gmail.com>
2023-07-27 01:24:35 +0900

    fragment_record.kt에 버튼 연결

--- app/src/main/java/com/example/nota/fragment_record.kt
+++ app/src/main/java/com/example/nota/fragment_record.kt
@@ -1,12 +1,15 @@
 package com.example.nota
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -25,6 +28,14 @@ class fragment_record : Fragment() {
     private lateinit var viewPager_record: ViewPager2
     private lateinit var tabLayout_record: TabLayout
 
+    private lateinit var fb_record_floating: FloatingActionButton
+    private lateinit var fb_record_floating_c: FloatingActionButton
+    private lateinit var tv_record_floating_c: TextView
+    private lateinit var fb_record_floating_w: FloatingActionButton
+    private lateinit var tv_record_floating_w: TextView
+
+    var isRunning:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,12 +48,44 @@ class fragment_record : Fragment() {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_record, container, false)
 
+        //----------------------------------------------
+        fb_record_floating = view.findViewById(R.id.fb_record_floating)
+        fb_record_floating_c = view.findViewById(R.id.fb_record_floating_c)
+        tv_record_floating_c = view.findViewById(R.id.tv_record_floating_c)
+        fb_record_floating_w = view.findViewById(R.id.fb_record_floating_w)
+        tv_record_floating_w = view.findViewById(R.id.tv_record_floating_w)
 
+        fb_record_floating.setOnClickListener {
+            isRunning = !isRunning
+            if (isRunning) {
+                // 다른 플로팅액션버튼들과 텍스트뷰들을 보이도록 설정
+                fb_record_floating_c.visibility = View.VISIBLE
+                tv_record_floating_c.visibility = View.VISIBLE
+                fb_record_floating_w.visibility = View.VISIBLE
+                tv_record_floating_w.visibility = View.VISIBLE
+            } else {
+                // 다른 플로팅액션버튼들과 텍스트뷰들을 숨기도록 설정
+                fb_record_floating_c.visibility = View.GONE
+                tv_record_floating_c.visibility = View.GONE
+                fb_record_floating_w.visibility = View.GONE
+                tv_record_floating_w.visibility = View.GONE
+            }
+        }
+        fb_record_floating_c.setOnClickListener {
+            val intent = Intent(requireContext(), WriteCollectionActivity::class.java)
+            startActivity(intent)
+        }
+        fb_record_floating_w.setOnClickListener {
+            val intent = Intent(requireContext(), WriteWishActivity::class.java)
+            startActivity(intent)
+        }
     //탭바 화면 이동 구현
     //-------------------------------------------------------
         viewPager_record = view.findViewById(R.id.vp_record)
         tabLayout_record = view.findViewById(R.id.tl_record)
         return view
+
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -73,6 +116,8 @@ class fragment_record : Fragment() {
                 else -> tab.text = "Tab ${position + 1}"
             }
         }.attach()
+
+
     }
     //-------------------------------------------------------
 
