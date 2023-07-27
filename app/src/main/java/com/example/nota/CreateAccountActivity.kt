fee097c19a25ff43c497f089cdeb44b7c6312958
J3214 <hjm66336831@gmail.com>
2023-07-28 01:35:21 +0900

    회원가입과 로그인 파이어베이스로 연결해서 구현 CreateAccountActivity.kt LoginActivity.kt

--- app/src/main/java/com/example/nota/CreateAccountActivity.kt
+++ app/src/main/java/com/example/nota/CreateAccountActivity.kt
@@ -3,11 +3,71 @@ package com.example.nota
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import java.util.Locale
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    lateinit var editText_name: EditText
+    lateinit var editText_password: EditText
+    lateinit var editText_passwordVerify: EditText
+    lateinit var button_logIn: Button
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.log_in)
+        editText_name = findViewById(R.id.editText_name)
+        editText_password = findViewById(R.id.editText_password)
+        editText_passwordVerify = findViewById(R.id.editText_passwordVerify)
+        button_logIn = findViewById(R.id.button_logIn)
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+        button_logIn.setOnClickListener {
+            val email = editText_name.text.toString().trim()
+            val password = editText_password.text.toString().trim()
+            val password_Verify = editText_passwordVerify.text.toString().trim()
+
+            // Validate...
+            if( password.equals(password_Verify)){
+            createUser(email, password)
+            }else{
+                Toast.makeText(this, "비밀번호 확인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    var intent = Intent(this, LoginActivity:: class.java)
+                    startActivity(intent)
+
+                    // 후에 반응
+                } else {
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                    // 실패 후 화면 반응?
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/nota/LoginActivity.kt
+++ app/src/main/java/com/example/nota/LoginActivity.kt
@@ -3,11 +3,65 @@ package com.example.nota
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var btn_login_login: Button
+    lateinit var et_login_name: EditText
+    lateinit var et_login_pw: EditText
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        et_login_name = findViewById(R.id.et_login_name)
+        et_login_pw = findViewById(R.id.et_login_pw)
+        btn_login_login = findViewById(R.id.btn_login_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_login_login.setOnClickListener(object: View.OnClickListener  {
+            override fun onClick(p0: View?) {
+                val email:String = et_login_name.text.toString()
+                val pw:String = et_login_pw.text.toString()
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)) {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "이메일과 비밀번호는 필수 입력사항 입니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
+                val auth = FirebaseAuth.getInstance()
+                auth.signInWithEmailAndPassword(email, pw).addOnCompleteListener(this@LoginActivity,
+                    object : OnCompleteListener<AuthResult> {
+                        override fun onComplete(p0: Task<AuthResult>) {
+                            if (p0.isSuccessful) {
+                                Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                    .putExtra("userId", auth.uid)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(this@LoginActivity, "이메일 또는 비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    })
+            }
+
+        })
     }
-}
+
+    }
+z
