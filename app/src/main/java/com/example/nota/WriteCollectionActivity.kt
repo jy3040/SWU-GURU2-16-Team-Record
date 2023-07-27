451ad102b6944c741e5236641e2cf777d671bc35
eileenjjy <dosajjy@naver.com>
2023-07-28 03:47:02 +0900

    스피너 기능 추가 (카테고리 명)

--- app/src/main/java/com/example/nota/WriteCollectionActivity.kt
+++ app/src/main/java/com/example/nota/WriteCollectionActivity.kt
@@ -3,6 +3,8 @@ package com.example.nota
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class WriteCollectionActivity : AppCompatActivity() {
 
@@ -10,9 +12,15 @@ class WriteCollectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.write_collection)
 
-        // 여기에 WriteCollectionActivity에 필요한 추가적인 설정이나 로직을 작성하세요.
-        // 예를 들어, EditText, 버튼 등의 UI 구성 요소를 추가하거나 필요한 작업을 처리할 수 있습니다.
-        // 또는 사용자 상호작용을 처리하고 필요한 데이터를 저장하는 등의 작업을 수행할 수 있습니다.
+        val spinner = findViewById<Spinner>(R.id.spinner_category)
+
+        // 카테고리명 배열 가져오기
+        val categories = resources.getStringArray(R.array.categories)
+
+        // 스피너 어댑터 설정
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 
     fun onBackButtonClicked(view: View) {
--- app/src/main/java/com/example/nota/WriteWishActivity.kt
+++ app/src/main/java/com/example/nota/WriteWishActivity.kt
@@ -3,12 +3,25 @@ package com.example.nota
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class WriteWishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.write_wish)
+
+        val spinner = findViewById<Spinner>(R.id.spinner_category)
+
+        // 카테고리명 배열 가져오기
+        val categories = resources.getStringArray(R.array.categories)
+
+        // 스피너 어댑터 설정
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
+
     fun onBackButtonClicked(view: View) {
         finish()
     }
