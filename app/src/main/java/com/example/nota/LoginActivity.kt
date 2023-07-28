094ebe8aed1522eadf342bcb67d2fa200aae9ccf
eileenjjy <dosajjy@naver.com>
2023-07-28 16:49:28 +0900

    자동 로그인

--- app/src/main/java/com/example/nota/LoginActivity.kt
+++ app/src/main/java/com/example/nota/LoginActivity.kt
@@ -12,6 +12,8 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import android.content.SharedPreferences
+
 
 class LoginActivity : AppCompatActivity() {
     lateinit var btn_login_login: Button
@@ -19,49 +21,79 @@ class LoginActivity : AppCompatActivity() {
     lateinit var et_login_pw: EditText
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var sharedPreferences: SharedPreferences
 
+    private fun saveLoginStatus(userId: String) {
+        val sharedPreferences = getSharedPreferences("login_status", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("user_id", userId)
+        editor.apply()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        auth = FirebaseAuth.getInstance()
+        sharedPreferences = getSharedPreferences("login_status", MODE_PRIVATE)
+
+        val userId = sharedPreferences.getString("user_id", null)
+        if (userId != null) {
+            // 이미 로그인한 사용자라면 메인 화면으로 바로 이동
+            val intent = Intent(this@LoginActivity, MainActivity::class.java).apply {
+                putExtra("userId", userId)
+            }
+            startActivity(intent)
+            finish()
+            return
+        }
 
         et_login_name = findViewById(R.id.et_login_name)
         et_login_pw = findViewById(R.id.et_login_pw)
         btn_login_login = findViewById(R.id.btn_login_login)
 
-        auth = FirebaseAuth.getInstance()
+        btn_login_login.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    val email: String = et_login_name.text.toString()
+                    val pw: String = et_login_pw.text.toString()
 
-        btn_login_login.setOnClickListener(object: View.OnClickListener  {
-            override fun onClick(p0: View?) {
-                val email:String = et_login_name.text.toString()
-                val pw:String = et_login_pw.text.toString()
-
-                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)) {
-                    Toast.makeText(
-                        this@LoginActivity,
-                        "이메일과 비밀번호는 필수 입력사항 입니다.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return
+                    if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)) {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "이메일과 비밀번호는 필수 입력사항 입니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                    }
+                    val auth = FirebaseAuth.getInstance()
+                    auth.signInWithEmailAndPassword(email, pw)
+                        .addOnCompleteListener(this@LoginActivity, object : OnCompleteListener<AuthResult> {
+                                override fun onComplete(p0: Task<AuthResult>) {
+                                    if (p0.isSuccessful) {
+                                        Toast.makeText(
+                                            this@LoginActivity,
+                                            "로그인 성공",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        saveLoginStatus(auth.uid ?: "")
+                                        val intent =
+                                            Intent(this@LoginActivity, MainActivity::class.java)
+                                                .putExtra("userId", auth.uid)
+                                        startActivity(intent)
+                                        finish()
+                                    } else {
+                                        Toast.makeText(
+                                            this@LoginActivity,
+                                            "이메일 또는 비밀번호가 다릅니다.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+                            })
                 }
-                val auth = FirebaseAuth.getInstance()
-                auth.signInWithEmailAndPassword(email, pw).addOnCompleteListener(this@LoginActivity,
-                    object : OnCompleteListener<AuthResult> {
-                        override fun onComplete(p0: Task<AuthResult>) {
-                            if (p0.isSuccessful) {
-                                Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
-                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                                    .putExtra("userId", auth.uid)
-                                startActivity(intent)
-                                finish()
-                            } else {
-                                Toast.makeText(this@LoginActivity, "이메일 또는 비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    })
-            }
 
-        })
-    }
+            })
+        }
     fun onBackButtonClicked(view: View) {
         finish()
     }
--- app/src/main/java/com/example/nota/fragment_setting.kt
+++ app/src/main/java/com/example/nota/fragment_setting.kt
@@ -1,9 +1,11 @@
 package com.example.nota
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class fragment_setting: Fragment() {
@@ -19,9 +21,24 @@ class fragment_setting: Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.setting, container, false)
-
-
+        val logoutButton = view.findViewById<Button>(R.id.button_logout)
+        logoutButton.setOnClickListener {
+            logoutUser()
+        }
         return view
+    }
+    private fun logoutUser() {
+
+        // 자동 로그인 설정에서 로그인 정보 삭제
+        val sharedPreferences = requireContext().getSharedPreferences("login_status", 0)
+        val editor = sharedPreferences.edit()
+        editor.remove("user_id")
+        editor.apply()
+
+        // 로그인 화면으로 이동
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 
 }
